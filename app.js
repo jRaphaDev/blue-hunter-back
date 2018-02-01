6c4cc1816b910ce8e900e57c87b14b5ab66e094e
Raphael Freitas <dev.rapha@gmail.com>
2018-02-01 00:08:49 -0200

    adjusting cors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 import router from './routers/index';
 import db from './config/db.mongoose';
 
 import seeds from './db/index';
 
-const app = express();
+const app = express()
+  .use(cors())
+  .use('/', router);
 
 const API_PORT = process.env.API_PORT || 3000;
 
-app
-  .get('/', (req, res) => res.send({ msg: 'Everything ok !!!'}) )
-  .use('/', router);
+app.get('/', (req, res) => res.send({ msg: 'Everything ok !!!'}) )
 
 app.listen(API_PORT, () => console.log(`API is running on port : ${API_PORT}`));
