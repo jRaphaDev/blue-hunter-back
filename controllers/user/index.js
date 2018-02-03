9159772e339fb79d20bb32a9a89ac288d6308e5c
Raphael Freitas <dev.rapha@gmail.com>
2018-02-02 23:08:30 -0200

    book + user

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -4,20 +4,20 @@ function findUserByName (req, res) {
   
   const name = req.params.name;
 
-  service.findUserByName(name).then(user=> {
+  service.findUserByName(name).then( user=> {
 
     if (user.length) {
       console.log(`was found ${user.length} user(s) with name: ${name}.`);
-      return res.status(200).send({ data: user });
+      return res.status(200).send(user);
     }
 
     console.log('no content users');
-    return res.status(204).send({ data: user });
+    return res.status(200).send([]);
 
   }).catch(err => {
 
     console.log(`err: ${err}.`);
-    return res.status(500).send({ data: err });
+    return res.status(500).send(err);
 
   });
 
